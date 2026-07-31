98343316eb599d9fdf0a2de184585e93b9f2a028
farhan <el3317.farhanmahmood@gmail.com>
2026-07-31 20:19:27 +0530

    code setup for successful API integration

---
+++ backend/src/main/java/com/quizai/backend/controller/QuizController.java
@@ -0,0 +1,14 @@
+package com.quizai.backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class QuizController {
+
+    @GetMapping("/api/test")
+    public String test() {
+        return "Backend Running Successfully 🚀";
+    }
+
+}
